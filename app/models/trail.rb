e18691fdb000bb2697ed4e83e8bf0d4519015752
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-18 20:53:07 -0400

    Changed associations

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,4 +1,4 @@
 class Trail < ApplicationRecord
-  has_many :users
   has_many :reviews
+  has_many :users, through: :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :trails
   has_many :reviews
+  has_many :trails, through: :reviews
+
   has_secure_password
 end
