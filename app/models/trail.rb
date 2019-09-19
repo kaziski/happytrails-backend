08e1fa12a0235fc31121d5ee760edbdb8c593194
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-18 20:45:45 -0400

    Add review model, associate model relations, run migrations

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,2 +1,4 @@
 class Trail < ApplicationRecord
+  has_many :users
+  has_many :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :trails
+  has_many :reviews
   has_secure_password
 end
