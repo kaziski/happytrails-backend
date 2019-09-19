40d7e65e53807171bc54c510dd6a09c228ebb23f
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-18 21:57:29 -0400

    seeded some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,28 @@ john = User.create(name: "John", username: "Cookingpapajohn", password: "passwor
 jiji = User.create(name: "Jion", username: "Jiji", password: "password")
 keima = User.create(name: "Keima", username: "Keima", password: "password")
 
+#Trail
+trail_list = [
+  ['Scrubglass', 2.4],
+  ['Peters Lake', 2],
+  ['Frankfort Mineral Springs', 1.2],
+  ['Ohio community park', 0.8]
+]
 
+trail_list.each do |trail|
+  Trail.create(name: trail[0], length: trail[1])
+end
+
+#Review
+review_list = [
+  ['Very beautiful! Easy walking', 2, 1],
+  ['The trail near the creek is shady. Perfect for summer hikes', 1, 2],
+  ['The fall is beautiful especially after rain', 3, 4],
+  ['Easy paved path.', 4, 3],
+  ['Perfect for practicing riding bikes', 4, 1],
+  ['My kids love playing here.', 3, 2]
+]
+
+review_list.each do |review|
+  Review.create(comment: review[0], trail_id: review[1], user_id: review[2])
+end
