fd535017f395886d8c4096c875f45cf7f67ff8a4
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-20 07:45:35 -0400

    Added a new controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::API
+  include ActionController::Helpers
 
+  helpers do 
+    def current_user
+      # currently a mocked version of "being logged in"
+      User.first
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
 end
