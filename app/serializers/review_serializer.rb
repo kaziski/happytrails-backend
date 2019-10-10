0b4167f0d5433c4bc168eca587923274d52b9d35
Kazumi Karbowski <kazkazinus@gmail.com>
2019-10-10 18:26:35 -0400

    Added api_reviews key

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,6 @@
 class ReviewSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :comment, :api_trail_id, :api_trail_name, :api_trail_url, :user_id
+  attributes :comment, :api_trail_id, :api_trail_name, :api_trail_url, :user_id, :api_reviews
   belongs_to :trail, serializer: TrailSerializer
   #belongs_to :user :trail
   # attribute :trail do |review|
