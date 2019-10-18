12f622b901334cd5af526f33dec45d352724e65d
Kazumi Karbowski <kazkazinus@gmail.com>
2019-10-17 21:10:25 -0400

    Changed things around for surge and heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # origins '*'
     #this specifies the front end url. * means any url
 
-    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'
+    origins 'http://localhost:3001', 'https://happytrails.surge.sh'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
